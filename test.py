ad56b1c50eb71f4885a75766ff74e645e675e6b7
AshokB <Ashok.Balusu@HCAHealthcare.com>
2024-02-07 10:34:00 -0600

    tests

---
+++ test.py
@@ -0,0 +1,10 @@
+import query
+# from google.cloud import spanner
+
+# query.query_data('stable-id', 'stable_id')
+# query.query_sipv_data_and_insert_to_new_instance_db('stable-id', 'stable_id','stable-id-test','stable_id_test_db')
+query.query_sir_data_and_insert_to_new_instance_db('stable-id', 'stable_id','stable-id-test','stable_id_test_db')
+
+'''
+https://github.com/googleapis/python-spanner/blob/122ab3679184276eb15e9629ca09956229f4b53a/samples/samples/snippets.py#L1584
+'''
